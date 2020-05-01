1498ec51ab9397727452d2d062252a32816c9f73
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-02 04:20:52 +0430

    clean up the file and import the 'asyncController' to handle the 'fetchData' async function

--- fetch.js
+++ fetch.js
@@ -1,38 +1,18 @@
 var fetchData = require('./helper/fetch-data');
 
-function asyncController(asyncFunction) {
-  var asyncHanlder = {};
-  // var _callback = null;
-  var _onSuccess = null;
-  var _onError = null;
+var asyncController = require('./async-controller');
 
-  asyncFunction(function (data) {
-    // resolve(data);
-    if (data) {
+function borrowFunction(resolve, reject) {
+
+  fetchData(function (data) {
+    if(data) {
       resolve(data);
     } else {
       reject('something is wrong!!!');
     }
   });
 
-  function resolve(data) {
-    // _callback(data);
-    _onSuccess(data);
-  }
-  function reject(err) {
-    _onError(err);
-  }
-
-  // asyncHanlder.done = function (callback) {
-  //   _callback = callback;
-  // }
-  asyncHanlder.then = function (onSuccess, onError) {
-    _onSuccess = onSuccess;
-    _onError = onError;
-  }
-
-  return asyncHanlder;
 }
 
-var asyncHanlder = asyncController(fetchData);
-module.exports = asyncHanlder;
+var asyncFetchData = asyncController(borrowFunction);
+module.exports = asyncFetchData;
