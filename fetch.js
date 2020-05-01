36629bff890d4349e5c970f8aae972d9a5d8003b
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-02 03:45:58 +0430

    change the 'undefined' value to an object

--- fetch.js
+++ fetch.js
@@ -1,9 +1,11 @@
 var fetchData = require('./helper/fetch-data');
 
-var fetchResult = undefined;
+// var fetchResult = undefined;
+var fetchResult = {};
 
 fetchData(function (data) {
-  fetchResult = data;
+  // fetchResult = data;
+  fetchResult.data = data;
 });
 
 module.exports = fetchResult;
