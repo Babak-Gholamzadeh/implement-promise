cc5ca30dc585ef84d5af37b203d739e52d23d602
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-02 04:02:31 +0430

    define resolve method to call the callback after resolving

--- fetch.js
+++ fetch.js
@@ -1,15 +1,19 @@
 var fetchData = require('./helper/fetch-data');
 
 var fetchResult = {
-  data: null,
+  // data: null,
+  resolve: function(data) {
+    this.callback(data);
+  },
   done: function(callback) {
-    callback(this.data);
+    // callback(this.data);
+    this.callback = callback;
   }
 };
 
 fetchData(function (data) {
-  fetchResult.data = data;
-  // fetchResult.done(data);
+  // fetchResult.data = data;
+  fetchResult.resolve(data);
 });
 
 module.exports = fetchResult;
