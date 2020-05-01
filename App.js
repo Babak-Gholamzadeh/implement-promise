9941fd3625852fed9bf679dcb9a229bb2f5fa180
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-02 03:59:06 +0430

    send a callback to 'done' method to retrieve the data

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 var fetchResult = require('./fetch.js');
 
-// setTimeout(function() {
-//   console.log(fetchResult);
-// }, 1000);
+// fetchResult.done = function(data) {
+//   console.log(data);
+// };
 
-fetchResult.done = function(data) {
-  console.log(data); // output> data is here!!!
-};
+fetchResult.done(function(data) {
+  console.log(data); // output> null
+});
