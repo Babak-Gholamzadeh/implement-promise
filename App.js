2ea28aa270ac6be28347b68c95539758313b13df
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-02 04:10:26 +0430

    call a 'then' method and pass two callbacks to it to handle the possible errors

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 var fetchResult = require('./fetch.js');
 
-fetchResult.done(function(data) {
-  console.log(data); // output> data is here!!!
-});
+// fetchResult.done(function (data) {
+//   console.log(data);
+// });
+
+// 'then' method is just the same as 'done' method
+// but takes two callbacks instead of one
+fetchResult.then(
+  // if data be fetched successfuly, then 'onSuccess' would be called
+  onSuccess,
+  // if any error occurs, then 'onError' would be called
+  onError
+);
