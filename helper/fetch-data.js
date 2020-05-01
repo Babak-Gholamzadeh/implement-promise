8d77c3f23de9a661c0ed43c77c7dd64ee7bcd8ce
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-02 03:39:09 +0430

    add a helper to mock the fetchData function

---
+++ helper/fetch-data.js
@@ -0,0 +1,12 @@
+function fetchData(fn) {
+  setTimeout(() => {
+
+    // Send some data to 'fn' function for resolving value
+    fn('data is here,');
+
+    // Send nothing to it for rejection purpos
+    // fn();
+
+  }, 500);
+}
+module.exports = fetchData;
