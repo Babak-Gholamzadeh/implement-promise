6f45b4e9a8b0a24d7d82791efc2b6057cfc7f95a
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-02 05:37:24 +0430

    test 'catch' method to handle an error and return a value to the next 'then' method

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 var asyncFetchData = require('./fetch.js');
-
 asyncFetchData
   .then(function (result) {
-    console.log('result 1:', result); // output> result 1: data is here!
+    console.log(result); // output> data is here!
+    throw 'something is wrong!';
     return result + ' hooray!';
   })
   .then(function (result) {
-    console.log(result); // output> data is here, hooray!
-  });
-
-asyncFetchData
-  .then(function (result) {
-    console.log('result 2:', result); // output> result 2: data is here!
+    console.log(result);
+  })
+  .catch(function(err) {
+    console.log(err); // output> something is wrong!
+    return 'everything is find now!';
+  })
+  .then(function(result) {
+    console.log(result); // output> everything is find now!
   });
