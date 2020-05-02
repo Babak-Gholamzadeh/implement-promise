cac1ea1210da5f50962b5c226c5c579eb56a45ba
Babak Gholamzadeh <babak.gholamzadeh92@gmail.com>
2020-05-02 04:35:56 +0430

    set the desire goal

--- App.js
+++ App.js
@@ -1,22 +1,24 @@
-var fetchResult = require('./fetch.js');
-
 var asyncFetchData = require('./fetch.js');
 
-asyncFetchData.then(
-  onSuccess,
-  onError
-);
-
-setTimeout(function() {
-  asyncFetchData.then(
-    onSuccess,
-    onError
+asyncFetchData
+  .then(function (result) {
+    console.log(result); // output> data is here,
+    return result + ' hooray!';
+  })
+  .then(function (result) {
+    console.log(result); // output> data is here, hooray!
+    return reverse(result);
+  })
+  .then(function (result) {
+    console.log(result); // output> !yarooh ,ereh si atad
+    throw 'something is wrong!';
+    return result + ' hooray!';
+  })
+  .then(
+    function (result) {
+      console.log(result);
+    },
+    function (err) {
+      console.log(err); // output> something is wrong!
+    },
   );
-}, 60_000 /* even after 1 minute */);
-
-function onSuccess(data) {
-  console.log('Result:', data); // output> Result: data is here!!!
-}
-function onError(err) {
-  console.log('Error:', err); // output> Error: something is wrong!!!
-}
